47d816860b577adf786dad38cf5341986b3866df
Luca Schutzenhofer <lucaschutzenhofer@hotmail.com>
2021-12-01 19:20:54 -0300

    editando estilo do botão

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -11,6 +11,8 @@ import styled from 'styled-components';
 const $Button = styled.button`
   background: ${props => props.theme.secondary};
   border-radius: 15px;
+  font-size: 16px;
+  text-transform: uppercase;
   padding: 1rem 3rem;
   color: ${props => props.theme.colors.white3};
   font-weight: ${props => props.theme.fonts.weight.bold};
