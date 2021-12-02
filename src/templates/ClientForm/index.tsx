0f122df3be60e22893d26c94a63244a297f13cc6
Luca Schutzenhofer <lucaschutzenhofer@hotmail.com>
2021-12-02 19:49:44 -0300

    criando form de cliente

---
+++ src/templates/ClientForm/index.tsx
@@ -0,0 +1,50 @@
+/**
+ * IMPORTS
+ */
+import { Field } from "components/form/field";
+import { Input } from "components/form/input";
+import { Row } from "components/form/row";
+import { IClientFormProps } from "./index.d";
+
+/**
+ * I am the client form
+ */
+export const ClientForm = (props: IClientFormProps) => {
+
+    return (
+        <>
+            <Field error={props.errors.fullname?.message} label="Nome Completo">
+                <Input name="fullname" innerRef={props.register} defaultValue={props.client?.fullname} />
+            </Field>
+            <Row>
+                <Field error={props.errors.email?.message} label="Email">
+                    <Input name="email" innerRef={props.register} defaultValue={props.client?.email} />
+                </Field>
+
+                <Field error={props.errors.phone?.message} label="Celular">
+                    <Input
+                        control={props.control}
+                        defaultValue={!!props.client?.phone && props.client?.phone}
+                        mask="(99) 99999-9999"
+                        formatChars={{ 9: '[0-9]' }}
+                        maskChar={null}
+                        name="phone"
+                    />
+                </Field>
+            </Row>
+
+            {!props.isEditing &&
+                <>
+                    <Field error={props.errors.password?.message} label="Senha">
+                        <Input name="password" innerRef={props.register} type="password" />
+                    </Field>
+
+                    <Field error={props.errors.confirmPassword?.message} label="Confirmar Senha">
+                        <Input name="confirmPassword" innerRef={props.register} type="password" />
+                    </Field>
+                </>
+            }
+
+        </>
+    )
+}
