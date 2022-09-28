709b695edd9f6db17c88bb8590a5240d20e56634
Luca Schutzenhofer <lucaschutzenhofer@hotmail.com>
2022-09-28 17:38:39 -0300

    fix address creation and update

--- src/templates/ProviderForm/index.tsx
+++ src/templates/ProviderForm/index.tsx
@@ -3,6 +3,7 @@
  */
 import { Field } from "components/form/field";
 import { Input } from "components/form/input";
+import { Row } from "components/form/row";
 import { Select } from "components/form/select";
 import { formatInputDate } from "utils/dateUtils";
 import { IProviderFormProps } from "./index.d";
@@ -16,39 +17,42 @@ export const ProviderForm = (props: IProviderFormProps) => (
             <Input name="fullname" innerRef={props.register} defaultValue={props.provider?.fullname} />
         </Field>
 
-        <Field error={props.errors.cpf?.message} label="CPF">
-            <Input
-                name="cpf"
-                control={props.control}
-                mask="999.999.999-99"
-                formatChars={{ 9: '[0-9]' }}
-                maskChar={null}
-                defaultValue={props.provider?.cpf}
-            />
-        </Field>
-        <Field error={props.errors.birthDate?.message} label="Data de Nascimento">
-            <Input
-                name="birthDate"
-                innerRef={props.register}
-                type="date"
-                defaultValue={formatInputDate(props.provider?.birthDate)}
-            />
-        </Field>
+        <Row>
+            <Field error={props.errors.cpf?.message} label="CPF">
+                <Input
+                    name="cpf"
+                    control={props.control}
+                    mask="999.999.999-99"
+                    formatChars={{ 9: '[0-9]' }}
+                    maskChar={null}
+                    defaultValue={props.provider?.cpf}
+                />
+            </Field>
+            <Field error={props.errors.birthDate?.message} label="Data de Nascimento">
+                <Input
+                    name="birthDate"
+                    innerRef={props.register}
+                    type="date"
+                    defaultValue={formatInputDate(props.provider?.birthDate)}
+                />
+            </Field>
+        </Row>
+        <Row>
+            <Field error={props.errors.email?.message} label="Email">
+                <Input name="email" innerRef={props.register} defaultValue={props.provider?.email} />
+            </Field>
 
-        <Field error={props.errors.email?.message} label="Email">
-            <Input name="email" innerRef={props.register} defaultValue={props.provider?.email} />
-        </Field>
-
-        <Field error={props.errors.phone?.message} label="Celular">
-            <Input
-                control={props.control}
-                defaultValue={!!props.provider?.phone && props.provider?.phone}
-                mask="(99) 99999-9999"
-                formatChars={{ 9: '[0-9]' }}
-                maskChar={null}
-                name="phone"
-            />
-        </Field>
+            <Field error={props.errors.phone?.message} label="Celular">
+                <Input
+                    control={props.control}
+                    defaultValue={!!props.provider?.phone && props.provider?.phone}
+                    mask="(99) 99999-9999"
+                    formatChars={{ 9: '[0-9]' }}
+                    maskChar={null}
+                    name="phone"
+                />
+            </Field>
+        </Row>
 
         <Field error={props.errors.category?.message} label="Categoria de Serviço">
             <Select
